859c8214f450e97d8fc68b08a786717f04638c9d
Aksel Glyholt <me@akselglyholt.com>
2025-02-13 00:53:42 +0100

    Added config file

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-# What's this?
+# VelocityLimboHandler
+A simple plugin for handling your fallback server.
+
 This is a velocity plugin for standalone Limbo servers under your network.
 It handles automatic reconnecting to the players previous server or to the lobby server.
 
@@ -7,8 +9,56 @@ It handles reconnection with a queue, so players return in a systematic order, s
 I developed this plugin to work with [Limbo by LOOHP](https://github.com/LOOHP/Limbo), but it should work with any Limbo server. But you might have to set up your limbo server differently depending on how it handles players and reconnection.
 
 
-# Installation
-Simply download the latest release from [releases](https://github.com/akselglyholt/velocity-limbo-handler)
-Then drag it into your velocity plugins folder, **NOT ON YOUR BACKEND SERVER!**
+## Installation
+1. Download the [latest release](https://github.com/AkselGlyholt/velocity-limbo-handler/releases/latest).
+2. Upload it into the plugins directory of your instance of Velocity.
+3. Restart the proxy to load the plugin and create the configuration files.
+4. Configure the proxy, limbo server and plugin as outlined below.
+5. Restart the proxy once again
 
-Now edit the config to your liking
+**NOTE:** It's very much recommend you disable `/server` command inside the limbo server, with your permissions manager. I recommend [LuckPerms](https://luckperms.net/) for this, because of their [context](https://luckperms.net/wiki/Usage#context) function
+
+## Configuration
+**NOTE:** It's very much recommend you disable `/server` command inside the limbo server, with your permissions manager. I recommend [LuckPerms](https://luckperms.net/) for this, because of their [context](https://luckperms.net/wiki/Usage#context) function
+
+### Proxy/Limbo
+VelocityLimboHandler does not implement a Fallback server so you will need to configure one yourself and add it to your velocity network.
+I recommend using [Limbo by LOOHP](https://github.com/LOOHP/Limbo), as I developed this plugin with that server in mind, but any standalone server should work fine!
+
+After you've set up the server and added it to your velocity network you will need to add it to your connection order, so that if the player couldn't connect to the first server they will be sent to the limbo server
+
+Example using the regular try orders:
+```toml
+[servers]
+  ...
+  try = ["default", "limbo"]
+```
+
+Example of using forced hosts
+```toml
+[forced-hosts]
+  "build.example.com" = ["build", "limbo"]
+  "pvp.example.com" = ["pvp", "limbo"]
+```
+
+### Plugin
+After following the installation guide, you can configure the plugin file
+`plugins/velocity-limbo-handler/config.yml`
+
+#### Required
+##### limbo-name
+* **Default:** Limbo
+* **Description:** Name of the limbo server you want this plugin to target
+
+##### direct-connect-server
+* **Default:** default
+* **Description:** Name of the server players should be connected to if they directly connect to the server and were sent to the limbo server
+
+#### Optional
+##### task-interval
+* **Default:** 3
+* **Description:** How often the plugin should check if it's possible to connect to the server again in seconds
+
+##### queue-notify-interval
+* **Default:** 30
+* **Description:** How often the player should be notified of their position in the queue
--- src/main/java/com/akselglyholt/velocityLimboHandler/VelocityLimboHandler.java
+++ src/main/java/com/akselglyholt/velocityLimboHandler/VelocityLimboHandler.java
@@ -8,13 +8,25 @@ import com.velocitypowered.api.event.EventManager;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import net.kyori.adventure.text.Component;
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.route.Route;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
-import java.util.Collection;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
@@ -28,12 +40,34 @@ public class VelocityLimboHandler {
 
     private static PlayerManager playerManager;
 
+    private static YamlDocument config;
+
     private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     @Inject
-    public VelocityLimboHandler(ProxyServer server, Logger loggerInstance) {
+    public VelocityLimboHandler(ProxyServer server, Logger loggerInstance, @DataDirectory Path dataDirectory) {
         proxyServer = server;
         logger = loggerInstance;
+
+        try {
+            config = YamlDocument.create(new File(dataDirectory.toFile(), "config.yml"),
+                    Objects.requireNonNull(getClass().getResourceAsStream("/config.yml")),
+                    GeneralSettings.DEFAULT,
+                    LoaderSettings.builder().setAutoUpdate(true).build(),
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder().setVersioning(new BasicVersioning("file-version"))
+                            .setOptionSorting(UpdaterSettings.OptionSorting.SORT_BY_DEFAULTS).build()
+            );
+
+            config.update();
+            config.save();
+        } catch (IOException e) {
+            logger.severe("Something went wrong while trying to update/create config: " + e);
+            logger.severe("Plugin will now shut down!");
+            Optional<PluginContainer> container = proxyServer.getPluginManager().getPlugin("velocity-limbo-handler");
+            container.ifPresent(pluginContainer -> pluginContainer.getExecutorService().shutdown());
+        }
+
         playerManager = new PlayerManager();
     }
 
@@ -57,6 +91,10 @@ public class VelocityLimboHandler {
         return playerManager;
     }
 
+    public static YamlDocument getConfig() {
+        return config;
+    }
+
     @Subscribe
     public void onInitialize(ProxyInitializeEvent event) {
         logger.info("Loading Limbo Handler!");
@@ -76,43 +114,43 @@ public class VelocityLimboHandler {
         // TODO: Add listeners here!
         eventManger.register(this, new ConnectionListener());
 
+        int reconnectInterval = config.getInt(Route.from("task-interval"));
+        int queueInterval = config.getInt(Route.from("queue-notify-interval"));
+
+        logger.info("Intervals " + reconnectInterval + " " + queueInterval);
+
         // Schedule the reconnection task
         proxyServer.getScheduler().buildTask(this, () -> {
-                    // Prevent NullPointerException when Queue is empty
-                    if (!playerManager.hasQueuedPlayers()) return;
+            // Prevent NullPointerException when Queue is empty
+            if (!playerManager.hasQueuedPlayers()) return;
 
-                    Player nextPlayer = playerManager.getNextQueuedPlayer();
+            Player nextPlayer = playerManager.getNextQueuedPlayer();
 
-                    if (nextPlayer == null || !nextPlayer.isActive()) return;
+            if (nextPlayer == null || !nextPlayer.isActive()) return;
 
-                    RegisteredServer previousServer = playerManager.getPreviousServer(nextPlayer);
+            RegisteredServer previousServer = playerManager.getPreviousServer(nextPlayer);
 
-                    // If enabled, check if a server responds to pings before connecting
-                    try {
-                        // TODO: Add config check for pinging servers for now just ping
-                        try {
-                            previousServer.ping().join(); // Check if the server is online
-                        } catch (CompletionException completionException) {
-                            // Server failed to respond to ping request, return to prevent spam
-                            return;
-                        }
+            // If enabled, check if a server responds to pings before connecting
+            try {
+                // TODO: Add config check for pinging servers for now just ping
+                try {
+                    previousServer.ping().join(); // Check if the server is online
+                } catch (CompletionException completionException) {
+                    // Server failed to respond to ping request, return to prevent spam
+                    return;
+                }
 
-                        Utility.logInformational(String.format("Connecting %s to %s.", nextPlayer.getUsername(), previousServer.getServerInfo().getName()));
-                        nextPlayer.createConnectionRequest(previousServer).connect();
+                Utility.logInformational(String.format("Connecting %s to %s.", nextPlayer.getUsername(), previousServer.getServerInfo().getName()));
+                nextPlayer.createConnectionRequest(previousServer).connect();
 
-                        //playerManager.removePlayer(nextPlayer);
-                    } catch (CompletionException exception) {
-                        // Prevent console from being spammed when a server is offline and ping-check is disabled
-                    }
-                })
-                .repeat(3, TimeUnit.SECONDS)
-                .schedule();
+                //playerManager.removePlayer(nextPlayer);
+            } catch (CompletionException exception) {
+                // Prevent console from being spammed when a server is offline and ping-check is disabled
+            }
+        }).repeat(reconnectInterval, TimeUnit.SECONDS).schedule();
 
         // Schedule queue position notifier
         proxyServer.getScheduler().buildTask(this, () -> {
-
-            int queueSize = playerManager.getReconnectQueue().size();
-
             for (Player player : playerManager.getReconnectQueue()) {
 
                 if (!player.isActive()) {
@@ -126,6 +164,6 @@ public class VelocityLimboHandler {
 
                 player.sendMessage(miniMessage.deserialize("<yellow>Queue position: " + position));
             }
-        }).repeat(10, TimeUnit.SECONDS).schedule();
+        }).repeat(queueInterval, TimeUnit.SECONDS).schedule();
     }
 }
