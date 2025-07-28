ca4d37463ce2b203e19f073c55ae4d95ff0e188a
Aksel Glyholt <me@akselglyholt.com>
2025-07-28 15:56:43 +0200

    Only shadedJar downloaded

--- src/main/java/com/akselglyholt/velocityLimboHandler/VelocityLimboHandler.java
+++ src/main/java/com/akselglyholt/velocityLimboHandler/VelocityLimboHandler.java
@@ -44,7 +44,7 @@ import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-@Plugin(id = "velocity-limbo-handler", name = "VelocityLimboHandler", version = "1.4.0")
+@Plugin(id = "velocity-limbo-handler", name = "VelocityLimboHandler", authors = "Aksel Glyholt")
 public class VelocityLimboHandler {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(VelocityLimboHandler.class);
     private static ProxyServer proxyServer;
